788993c8fce48da3f929104dd312da0baac46b37
adam-86 <astabenow.dev@gmail.com>
2018-09-28 10:02:15 -0700

    fixed monthly conversion and added CPM

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,19 @@
 const inputs = document.querySelectorAll('.number-input'),
-  submit = document.querySelector('.submit'),
-  output = document.querySelector('.output');
+      submit = document.querySelector('.submit'),
+      output = document.querySelector('.output');
 
 function getResults() {
   //inputs
   let perDay = inputs[0].value,
-    perWeek = inputs[1].value,
-    perMonth = inputs[2].value,
+      perWeek = inputs[1].value,
+      perMonth = inputs[2].value,
+      monthRate = inputs[3].value,
 
     //outputs
     outputDays,
     outputWeeks,
-    outputMonths;
+    outputMonths,
+    outputCpm;
 
   if (perDay != 0) {
     outputDays = perDay;
@@ -21,12 +23,15 @@ function getResults() {
   } else if (perWeek != 0) {
     outputDays = perWeek / 7;
     outputWeeks = perWeek;
-    outputMonths = perWeek * 30;
+    outputMonths = perWeek * 4;
 
   } else if (perMonth != 0) {
     outputDays = perMonth / 30;
     outputWeeks = perMonth / 4;
     outputMonths = perMonth;
+
+  } if(monthRate != 0) {
+    outputCpm = Math.round(monthRate / outputMonths * 1000) / 100;
   }
 
   //output results rounded to the nearest interger
@@ -34,6 +39,7 @@ function getResults() {
       <span>Impressions Per Day:<strong> ${Math.round(outputDays)} </strong></span>
       <span>Impressions Per Week:<strong> ${Math.round(outputWeeks)} </strong></span>
       <span>Impressions Per Month:<strong> ${Math.round(outputMonths)} </strong></span>
+      <span>CPM:<strong> $ ${(outputCpm)} </strong></span>
       `;
 
   clearInputs();
